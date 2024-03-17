6926869a1cecfdc50f25e9abe20f9579fa169116
mohamedhossam2377 <m.hossam2377@gmail.com>
2024-03-16 17:07:36 -0700

    new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     elif idx >= len(my_list):
         return my_list
-    co_my_list = my_list()
+    co_my_list = my_list.copy()
     co_my_list[idx] = element
     return co_my_list
