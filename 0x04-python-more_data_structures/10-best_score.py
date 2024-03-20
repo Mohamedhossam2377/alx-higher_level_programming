3b246ee60e1c4f3a9fbd760f5e7b57a48e94feaf
mohamedhossam2377 <m.hossam2377@gmail.com>
2024-03-20 16:56:27 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
     max_v = 0
     max_k = None
-    for key, value in a_dictionary.item():
+    for key, value in a_dictionary.items():
         if value > max_v:
             max_v = value
             max_k = key
