5436f2f199d4b9e651a1080b18729d95a45306fe
mohamedhossam2377 <m.hossam2377@gmail.com>
2024-04-02 18:13:07 -0700

    sqaure

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""square module"""
 
 
 class Square:
+    """define square"""
 
     def __init__(self, size=0):
+        """size is length of side of the sqaure"""
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
