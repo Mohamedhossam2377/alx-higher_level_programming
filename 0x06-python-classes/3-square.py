146acf2c8350242200eebb9b029c46fcbe884ce8
mohamedhossam2377 <m.hossam2377@gmail.com>
2024-04-02 18:18:31 -0700

    square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """define square"""
+
+    def __init__(self, size=0):
+        """size is length of side of the sqaure"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """area of the square"""
+        return self.__size ** 2
